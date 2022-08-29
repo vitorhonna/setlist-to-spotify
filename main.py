74e418e8961379ec9970a8ccb39ccdb3213e38e7
Vitor Honna <vitorhonna@gmail.com>
2022-08-28 23:54:15 -0300

    setlist songs scoring working

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import setlistfm
 import utils
 
-setlists = setlistfm.get_setlists(
-    5, '6797c795-08b4-4da2-a4d8-95a554c2a91c', artistName='jao', cityName='')
+def main():
+    setlists = setlistfm.get_setlists(
+        3, 
+        '6797c795-08b4-4da2-a4d8-95a554c2a91c', 
+        artistName='jao', 
+        cityName='')
 
-utils.calculate_popularity(setlists)
+    songs_popularity = utils.calculate_popularity(setlists)
 
-# for setlist in setlists:
-#     print(setlist)
-#     utils.calculate_popularity(setlist)
+    # for setlist in setlists:
+    #     print(setlist)
+    #     utils.calculate_popularity(setlist)
+
+if __name__ == '__main__':
+    main()
--- utils.py
+++ utils.py
@@ -1,10 +1,18 @@
+def checkIfSongIsInList(songName, list):
+    for index, songFromList in enumerate(list):
+        if songFromList.name == songName:
+            return [True, index]
+    return [False, None]
+
+
 def calculate_popularity(setlists):
     # print(setlists)
+
     class Song:
         def __init__(self, name, relativePopularity):
             self.name = name
-            self.relativePopularity = relativePopularity
             self.count = 1
+            self.relativePopularity = relativePopularity
 
         def updateCount(self):
             self.count = self.count + 1
@@ -15,20 +23,58 @@ def calculate_popularity(setlists):
 
         def calculatePopularity(self):
             popularity = self.relativePopularity / self.count
+            print(round(popularity, 2))
             return popularity
 
-    songs = []
+        def lookInside(self):
+            print(f'{self.name}, {self.count}, {round(self.relativePopularity, 2)}')
 
-    setlist = setlists[1]
+    songsPopularity = []
 
-    for i in range(len(setlist)):
-        song = setlist[i].lower()
-        print(i+1, song)
+    for setlistPosition, setlist in enumerate(setlists):
+        print(setlist)
+        print('='*50)
+        for songPositionInSetlist, songName in enumerate(setlist):
+            songName = songName.lower()
 
-        song_popularity = {
-            'name': song,
-            'count':
-            ''
-        }
+            [songIsInList, index] = checkIfSongIsInList(
+                songName, songsPopularity)
 
-    return
+            if songIsInList:
+                # print(f'{songName} SIM, posicao {index}')
+                song = songsPopularity[index]
+                relativePopularity = (songPositionInSetlist+1) / len(setlist)
+                song.updateRelativePopularity(relativePopularity)
+                song.updateCount()
+            else:
+                # print(f'{songName} NAO')
+                relativePopularity = (songPositionInSetlist+1) / len(setlist)
+                song = Song(songName, relativePopularity)
+                songsPopularity.append(song)
+
+    for song in songsPopularity: 
+        song.lookInside()
+        # song.calculatePopularity()
+
+            # print(songPositionInSetlist+1, song.name)
+
+    # EXPECTED OUTPUT STRUCTURE
+    # songs_info = [
+    #     {
+    #         'name': 'clarão',
+    #         'relativePopularity': 0.01,
+    #         'count': 3,
+    #     },
+    #     {
+    #         'name': 'meninos e meninas',
+    #         'relativePopularity': 0.50,
+    #         'count': 3,
+    #     },
+    #     {
+    #         'name': 'idiota',
+    #         'relativePopularity': 0.99,
+    #         'count': 3,
+    #     },
+    # ]
+
+    return songsPopularity
